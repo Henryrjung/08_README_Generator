2c75c4a6ed6c18c938b64bfd65367332997e54e4
HenryJ <henryjung667@gmail.com>
2020-11-08 21:24:10 -0600

    updated questions

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generateMarkdown');
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'What is the project description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'please enter the table of contents:',
+        name: 'tableOfContents',
+    },
+    {
+        type: 'input',
+        message: 'Please enter instructions to install the project:',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'please enter instructions on how to use the project:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please choose a license',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Please enter how users can contribute to this project:',
+        name: 'contributions',
+    },
+    {
+        type: 'input',
+        message: 'Please enter how to test this project:',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Please enter questions for this project:',
+        name: 'questions',
+    },
 ];
 
 // function to write README file
