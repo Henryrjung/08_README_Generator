2b9cd958904cde041fbc7d9cfe0bd61bffd066a1
HenryJ <henryjung667@gmail.com>
2020-11-10 21:41:09 -0600

    updated markdown generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
+  ${data.description}
+
+  ## Table of Contents
+  * installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+  
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributing
+
+  ${data.contributions}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  ${data.questions}
 `;
 }
 
